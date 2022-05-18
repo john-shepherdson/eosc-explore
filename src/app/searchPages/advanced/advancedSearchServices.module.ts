586b81311a790648364a5eef5b92fffe312ec9cc
konstantina.galouni <kgalouni@di.uoa.gr>
2022-05-18 17:50:25 +0300

    [Aggregator | new-theme]:[Bug fix] advancedSearchServices.module.ts: Imported AdvancedSearchServicesRoutingModule instead of AdvancedSearchDataProvidersRoutingModule.

--- src/app/searchPages/advanced/advancedSearchServices.module.ts
+++ src/app/searchPages/advanced/advancedSearchServices.module.ts
@@ -2,15 +2,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 
-import {AdvancedSearchDataProvidersRoutingModule} from './advancedSearchDataProviders-routing.module';
 import {OpenaireAdvancedSearchServicesComponent} from './advancedSearchServices.component';
 
 import {SearchDataProvidersModule} from "../../openaireLibrary/searchPages/searchDataProviders.module";
+import {AdvancedSearchServicesRoutingModule} from "./advancedSearchServices-routing.module";
 
 @NgModule({
   imports: [
     CommonModule, FormsModule,
-    AdvancedSearchDataProvidersRoutingModule,
+    AdvancedSearchServicesRoutingModule,
     SearchDataProvidersModule
 
   ],
